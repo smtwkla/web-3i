3f3258d706e0b9f563fdd45fe4d43bb3d2ff19c7
kla <smtwkla@gmail.com>
2018-09-21 07:46:16 +0530

    working version

--- web-3i/app.py
+++ web-3i/app.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://web:iot_Data_2018@db.iot.mspvtiles.com/iotdata"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+class Channel_Data(db.Model):
+        __tablename__ = "channel_data"
+        id = db.Column(db.Integer, primary_key=True)
+        channel_id = db.Column(db.Integer, db.ForeignKey('channels.id'), nullable=False)
+        ts = db.Column(db.DateTime, nullable=False)
+        value = db.Column(db.Numeric(25, 6))
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    data = Channel_Data.query.filter_by(channel_id=2).order_by(Channel_Data.ts.desc()).first()
+    amps = data.value
+    ts = data.ts.isoformat()
+    return f'<h1>Paddle Amps:{amps}</h1>Timestamp: {ts}'
 
 
 if __name__ == '__main__':
