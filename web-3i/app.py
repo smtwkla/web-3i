55b3b2316ffad59258b7c5d625ba1904e5de3b2e
Lakshmi Anand K <smtwkla@gmail.com>
2018-09-21 15:45:29 +0530

    Update app.py

--- web-3i/app.py
+++ web-3i/app.py
@@ -19,7 +19,7 @@ def hello_world():
     data = Channel_Data.query.filter_by(channel_id=2).order_by(Channel_Data.ts.desc()).first()
     amps = data.value
     ts = data.ts.isoformat()
-    return f'<html><body><h1>Paddle Amps:{amps}</h1>Timestamp: {ts}<script  language="javascript" type="text/javascript" setTimeout(function() {{ window.location=window.location;}},5000);</script></body></html>'
+    return f'<html><head><meta http-equiv="refresh" content="3"></head>?<body><h1>Paddle Amps:{amps}</h1>Timestamp: {ts}<script  language="javascript" type="text/javascript" setTimeout(function() {{ window.location=window.location;}},5000);</script></body></html>'
 
 
 if __name__ == '__main__':
