d4adcdbcc84dee46b42c19dc2932a93318abc372
kla <smtwkla@gmail.com>
2018-10-04 07:35:51 +0530

    empty chart app using create-react-app

--- client/chart/src/ChannelChooser.js
+++ client/chart/src/ChannelChooser.js
@@ -1,10 +1,17 @@
 import {Component} from "react";
-import Button from "react-bootstrap/lib/Button";
 import React from "react";
+import DropdownButton from "react-bootstrap/lib/DropdownButton";
+import MenuItem from "react-bootstrap/lib/MenuItem";
+
 
 class ChannelChooser extends Component {
     render() {
-        return <Button  bsStyle="primary">Generate Graph</Button>
+        return <DropdownButton id={'dropdown-basic-Default'} key={1} bsStyle={'default'} title={'Channel 5'}>
+            <MenuItem eventKey={1}>Channel 1</MenuItem>
+            <MenuItem eventKey={2}>Channel 2</MenuItem>
+            <MenuItem eventKey={3}>Channel 3</MenuItem>
+            <MenuItem eventKey={4}>Channel 4</MenuItem>
+        </DropdownButton>
     }
 }
 
---
+++ client/chart/src/DisplayGraph.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+
+class DisplayGraph extends React.Component{
+    render(){
+        return <p>Graph here! {this.props.channel_id} </p>
+    }
+}
+
+export default DisplayGraph
