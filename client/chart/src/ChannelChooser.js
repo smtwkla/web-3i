ae8458b1533a6625a08e98e25c79a107f76942db
kla <smtwkla@gmail.com>
2018-10-04 06:44:17 +0530

    empty chart app using create-react-app

---
+++ client/chart/src/ChannelChooser.js
@@ -0,0 +1,11 @@
+import {Component} from "react";
+import Button from "react-bootstrap/lib/Button";
+import React from "react";
+
+class ChannelChooser extends Component {
+    render() {
+        return <Button  bsStyle="primary">Generate Graph</Button>
+    }
+}
+
+export default ChannelChooser
