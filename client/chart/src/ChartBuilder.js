d52b24610313ca0afc96fb5ad68d07c6a105c43e
kla <smtwkla@gmail.com>
2018-10-06 23:02:34 +0530

    channel data JSON download from AJAX - testing

--- client/chart/src/ChartBuilder.js
+++ client/chart/src/ChartBuilder.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { Grid, Row, Col } from 'react-bootstrap';
 import Button from "react-bootstrap/lib/Button";
-
+import Datetime from "react-datetime";
 import ChannelChooser from "./ChannelChooser";
 import DisplayGraph from "./DisplayGraph"
 
+import './react-datetime.css';
 
 class ChartBuilder extends React.Component{
 
@@ -27,10 +28,10 @@ class ChartBuilder extends React.Component{
                         <ChannelChooser channel_id={this.state.channel.id} onChannelChange={this.handleChannelChange} />
                     </Col>
                     <Col xs={4} md={4}>
-                        Start
+                        Start: <Datetime dateFormat={'DD-MM-YYYY'} timeFormat={'HH:mm:ss'}/>
                     </Col>
                     <Col xs={4} md={4}>
-                        End
+                        End: <Datetime dateFormat={'DD-MM-YYYY'} timeFormat={'HH:mm:ss'}/>
                     </Col>
                 </Row>
                 <Row className="show-grid">
@@ -53,7 +54,7 @@ class ChartBuilder extends React.Component{
             <Grid className="chart-box">
                 <Row className="show-grid">
                     <Col xs={12} md={12}>
-                        <DisplayGraph channel={this.state.channel} start={''} end={''} options={''} refresh={''} />
+                        <DisplayGraph channel={this.state.channel} start={'2018-10-06T12:00:00'} end={'2018-10-06T23:59:59'} options={''} refresh={''} />
                     </Col>
                 </Row>
             </Grid>
--- client/chart/src/DisplayGraph.js
+++ client/chart/src/DisplayGraph.js
@@ -34,18 +34,22 @@ class DisplayGraph extends React.Component{
             //fetch data
             const chartData = [['Timestamp', this.props.channel.name]]
 
-            axios.get('http://127.0.0.1:5000/channel_data/1')
+            axios.get('http://127.0.0.1:5000/channel_data/' + this.props.channel.id + "?from_ts=" +
+                this.props.start + '&to_ts='+ this.props.end)
                 .then(res => {
                     console.log(res.data);
                     let record = [];
-                    for(let i = 0; i<res.data.length; i++){
-                        record.push(
-                            [ new Date(res.data[i][0]) , res.data[i][1] ]
-                        );
+                    if (res.data.length===0) this.setState({status: 'empty'});
+                    else {
+                        for (let i = 0; i < res.data.length; i++) {
+                            record.push(
+                                [new Date(res.data[i][0]), res.data[i][1]]
+                            );
+                        }
+                        const data = chartData.concat(record);
+                        this.setState({status: 'loaded'});
+                        this.setState({data});
                     }
-                    const data = chartData.concat(record);
-                    this.setState({status: 'loaded'});
-                    this.setState({data});
                 });
 
         }
@@ -70,7 +74,14 @@ class DisplayGraph extends React.Component{
                     />
                 </div>
             )
-        } else {
+        }
+        else if (this.state.status ==='empty') {
+            return (
+                <div><h3>No data...</h3></div>
+            )
+
+        }
+        else{
             return (
                 <div><h3>Loading data...</h3></div>
             )
